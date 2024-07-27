002693f697abd50289b57377cef8417e180584d4
Aleeg0 <Sankafoxe@gmail.com>
2024-07-27 17:02:15 +0300

    add logic with opening modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import {Poster} from "./Components";
+import {LogForm, Poster} from "./Components";
 import "./Styles/index.scss"
+import {useState} from "react";
+
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const onOpenModal = () => {
+    setIsModalOpen(true);
+  }
+
+  const onCloseModal = () => {
+    setIsModalOpen(false);
+  }
+
   return (
     <>
-      <Poster/>
+      <LogForm
+        isOpen={isModalOpen}
+        onClose={onCloseModal}
+      />
+      <Poster
+        onButtonClick={onOpenModal}
+      />
     </>
   )
 }
