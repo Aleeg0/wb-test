fdc043ba30353f13fde8dcf6f0cac52eba231095
Aleeg0 <Sankafoxe@gmail.com>
2024-07-27 15:38:30 +0300

    add arrowSvg as component

---
+++ src/Components/SVGC/ArrowSvg.tsx
@@ -0,0 +1,10 @@
+export const ArrowSvg = () => {
+  return (
+    <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="44" height="44" rx="22" fill="white"/>
+      <rect x="4" y="4" width="36" height="36" rx="18" fill="white"/>
+      <path d="M17 17H27V27" stroke="#1F4284" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M17 27L27 17" stroke="#1F4284" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+};
